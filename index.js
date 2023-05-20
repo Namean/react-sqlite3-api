5230175b8a7d935feb07d85a4aba4a4dc37c3452
Namean <dwilsoncnc@gmail.com>
2023-05-20 16:50:20 -0400

    installed dependencies and made some corrections to index.js '/employees' route

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const cors = require('cors');
+
 app.use(cors());
 
-
+let dbName = "chinook.db"
+let databaseConnectionString = `./${dbName}`
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./src/db/chinook.db');
+const db = new sqlite3.Database(databaseConnectionString);
 
 let query = 'select * from employees';
 
@@ -33,16 +35,28 @@ app.get('/employees/:id', cors(), (req, res) => {
 
 	// Maybe write a function for this so its not hardcoded
 	// as the api may change in the future
-	let _id = req.orginalUrl.split('/')[2];
+
+	let failureMessage, successMessage;
+
+	failureMessage = 'something went wrong';
+	successMessage = 'success!';
+
+	let _id = req.originalUrl.split('/')[2];
+
+	console.log(req.originalUrl);
 
 	db.all(query, (err, row) => {
 		try {
-			let singleEmployee = row.filter(employee = {
+
+			let singleEmployee = row.filter(employee => {
 				return employee.EmployeeId == _id
 			});
+
 			res.json(singleEmployee);
 			console.log(successMessage);
-		} catch(error) {
+
+
+		} catch (error) {
 			console.log(failureMessage, error);
 
 		}
